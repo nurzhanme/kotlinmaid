39c38a7f1941fbb76199f8fd41b52ba398a29622
Nurzhan Aitbayev <nurzhan@outlook.com>
2023-10-25 19:47:10 +0600

    build.gradle.kts fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,17 @@ tasks.withType<Jar> {
     manifest {
         attributes["Main-Class"] = "MainKt"
     }
+
+    // To avoid the duplicate handling strategy error
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // To add all of the dependencies
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
 
 kotlin {
